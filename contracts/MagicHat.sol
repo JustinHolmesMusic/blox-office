34d2012f8c94543045e703115f2d800fcd3ee389
Jakub Hejhal <kubic71@gmail.com>
2024-07-06 19:22:42 +0200

    deploy contract

--- contracts/MagicHat.sol
+++ contracts/MagicHat.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract PaymentLog {
+contract MagicHat {
     // Struct to hold payment details
     bytes32[] public rabbitHashes;
 
---
+++ scripts/Deploy.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "../lib/forge-std/src/Script.sol";
+import "../contracts/MagicHat.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+
+contract DeployScript is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        bytes32[] memory rabbitHashes = new bytes32[](2);
+        // rabbitHashes[0] = keccak256(abi.encodePacked("rabbit1"));
+        // rabbitHashes[1] = keccak256(abi.encodePacked("rabbit2"));
+
+        rabbitHashes = new bytes32[](100);
+        for (uint256 i = 0; i < 100; i++) {
+            rabbitHashes[i] = keccak256(abi.encodePacked(Strings.toString(i)));
+        }
+
+
+        MagicHat magicHat = new MagicHat(rabbitHashes);
+
+        console.log("MagicHat deployed to:", address(magicHat));
+
+        vm.stopBroadcast();
+    }
+}

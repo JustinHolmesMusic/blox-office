11e4d37b1f94908e9ec3d9580205442644c0b789
jMyles <justin@justinholmes.com>
2024-07-16 18:50:07 +0200

    Test showing various set and show functionality.

--- test/SetlistTests.sol
+++ test/SetlistTests.sol
@@ -16,17 +16,29 @@ contract SetTest is Test {
         rabbitHashes[0] = keccak256(abi.encodePacked("rabbit1"));
         rabbitHashes[1] = keccak256(abi.encodePacked("rabbit2"));
 
+        uint16 artist_id = 0;
+        uint64 blockheight = 420;
+
         set_contract.addSet({
-            artist_id: 0,
-            blockheight: 420,
+            artist_id: artist_id,
+            blockheight: blockheight,
             shape: 0,
             order: 0,
             rabbitHashes: rabbitHashes
         });
-    }
 
-//    function testCommitSet() public {
-    // This will take a struct as an argument; implement then.
-//        set_contract.commitSet("1:1234", 0, 0, 100, ["rabbit1", "rabbit2"]);
-//    }
+        bytes32 showBytes = bytes32(abi.encodePacked(artist_id, blockheight));
+
+        ShowSet.Set memory this_set_in_particular = set_contract.getSetForShow(artist_id, blockheight, 0);
+        assertEq(this_set_in_particular.shape, 0);
+        bytes32 lets_see = set_contract.show_ids(0);
+        assertEq(lets_see, showBytes);
+
+        ShowSet.Set memory same_set_by_bytes = set_contract.getSetForShowByShowBytes(showBytes, 0);
+        assertEq(same_set_by_bytes.shape, this_set_in_particular.shape);
+        assertEq(same_set_by_bytes.order, this_set_in_particular.order);
+        assertEq(same_set_by_bytes.rabbitHashes[0], rabbitHashes[0]);
+        assertEq(same_set_by_bytes.rabbitHashes[1], rabbitHashes[1]);
+        
+    }
 }

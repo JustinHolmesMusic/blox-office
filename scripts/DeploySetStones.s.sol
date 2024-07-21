ef0194122e9bc5c69ef2276c53ffcb0ddcd61252
Jakub Hejhal <kubic71@gmail.com>
2024-07-21 16:16:25 +0200

    update DeploySetStones script

--- scripts/DeploySetStones.s.sol
+++ scripts/DeploySetStones.s.sol
@@ -7,19 +7,16 @@ import "../contracts/SetStone.sol";
 import "../lib/openzeppelin-contracts/contracts/utils/Strings.sol";
 
 
-contract DeployMagicHatScript is Script {
+contract DeploySetStonesScript is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-
-        LiveSet liveSet = new LiveSet();
-        console.log("LiveSet deployed to:", address(liveSet));
 
         // TODO: how to get the address of the deployer?
         // We somehow need to get the address of the deployer PRIVATE KEY
         address initialOwner = vm.addr(deployerPrivateKey);
 
-        SetStone setStone = new SetStone(address(liveSet), initialOwner, "https://justinholmes.com/setstones/");
+        SetStone setStone = new SetStone(initialOwner, "https://justinholmes.com/setstones/");
         console.log("SetStone deployed to:", address(setStone));
 
         vm.stopBroadcast();
